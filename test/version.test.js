1c82c88695b1c0f002f76d2cf62845df3fb356ec
lloyet <contact@lloyet.dev>
2023-03-28 11:04:23 +0200

    fix(ci): improve tests

--- test/version.test.js
+++ test/version.test.js
@@ -1,10 +1,13 @@
 const request = require("supertest");
+const assert = require("assert");
 const { app } = require("../index");
 
 request(app)
 	.get("/version")
 	.expect("Content-Type", /json/)
-	.expect(400)
+	.expect(200)
 	.end((err, res) => {
 		if (err) throw err;
+
+		assert.strictEqual(res.body.version, "1.0.0");
 	});
