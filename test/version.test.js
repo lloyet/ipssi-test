774977e1e546b7bc1efce4e8ed88901b0b3ad741
lloyet <contact@lloyet.dev>
2023-03-28 17:54:14 +0200

    feat(ci): improve with mocha

--- test/version.test.js
+++ test/version.test.js
@@ -1,13 +1,20 @@
 const request = require("supertest");
 const assert = require("assert");
-const { app } = require("../index");
+const { describe, it } = require("mocha");
+const { server } = require("../src/app");
 
-request(app)
-	.get("/version")
-	.expect("Content-Type", /json/)
-	.expect(200)
-	.end((err, res) => {
-		if (err) throw err;
+describe("Test version", () => {
+	it("Is version available", (done) => {
+		request(server)
+			.get("/version")
+			.expect("Content-Type", /json/)
+			.expect(200)
+			.end((err, res) => {
+				if (err) return done(err);
 
-		assert.strictEqual(res.body.version, "1.0.0");
+				assert.strictEqual(res.body.version, "1.0.0");
+
+				return done();
+			});
 	});
+});
